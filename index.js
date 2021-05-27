c341c54b15fe04f2fec9d9f8f7dd1a4222d485a3
VictorLCosta <victorlc2019@outlook.com>
2021-05-27 16:00:15 -0300

    Project base

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost:27017/scheduling", {useNewUrlParser: true, useUnifiedTopology: true})
+
+app.listen(8080, () => {
+    console.log("Server rodando");
+})
